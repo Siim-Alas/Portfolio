720231df4f8cdcd7de6ebcfb46bc3b19d5ca3165
Siim Alas <siim.alas@outlook.com>
2020-05-17 12:50:55 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let infoCollapsed = false;
+
+document.addEventListener('scroll', () => {
+    if (window.innerWidth <= 768) {
+        if (infoCollapsed) {
+            if (window.pageYOffset === 0) {
+                $('.collapse').collapse('show');
+                infoCollapsed = false;
+            }
+        } else {
+            if (window.pageYOffset > 0.2 * window.innerHeight) {
+                $('.collapse').collapse('hide');
+                infoCollapsed = true;
+            }
+        }
+    } else {
+        $('.collapse').collapse('show');
+        $('#sidebar').css('height', '100vh');
+        infoCollapsed = false;
+    }
+});
+
