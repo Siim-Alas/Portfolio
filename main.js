87887293c5fce8593aa91cea6b531765a00a1ed7
Siim Alas <siim.alas@outlook.com>
2020-05-18 15:41:28 +0300

    Made sidebar sticky setting apply on pageready and adjusted padding and implemented overflow hidden

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 
+function setSidebar() {
+    if (window.innerWidth <= 991) {
+        $('#sidebar').removeClass('sticky-top');
+    } else {
+        $('#sidebar').addClass('sticky-top');
+    }
+}
+
 $(document).ready(() => {
+    setSidebar();
     $('#copyrightP').html(`&copy; ${new Date().getFullYear()} - Siim Alas`);
+
     $(window).resize(() => {
-        if (window.innerWidth <= 991) {
-            $('#sidebar').removeClass('sticky-top');
-        } else {
-            $('#sidebar').addClass('sticky-top');
-        }
+        setSidebar();
     });
 });
