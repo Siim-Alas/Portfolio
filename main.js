5d6fb92230d15b19ac637e440e8affab4e2d27e5
Siim Alas <siim.alas@outlook.com>
2020-05-18 11:39:24 +0300

    Replaced sidebar collapse with adding and removing stycky top class

--- main.js
+++ main.js
@@ -1,27 +1,11 @@
 
-let infoCollapsed = false;
-
-document.addEventListener('scroll', () => {
-    if (window.innerWidth <= 991) {
-        if (infoCollapsed) {
-            if (window.pageYOffset === 0) {
-                $('.collapse').collapse('show');
-                infoCollapsed = false;
-            }
-        } else {
-            if (window.pageYOffset > 0.2 * window.innerHeight) {
-                $('.collapse').collapse('hide');
-                infoCollapsed = true;
-            }
-        }
-    } else {
-        $('.collapse').collapse('show');
-        $('#sidebar').css('height', '100vh');
-        infoCollapsed = false;
-    }
-});
-
 $(document).ready(() => {
     $('#copyrightP').html(`&copy; ${new Date().getFullYear()} - Siim Alas`);
+    $(window).resize(() => {
+        if (window.innerWidth <= 991) {
+            $('#sidebar').removeClass('sticky-top');
+        } else {
+            $('#sidebar').addClass('sticky-top');
+        }
+    });
 });
-
